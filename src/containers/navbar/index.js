18e451fce2ae40e8d4196b5c87d99c7919796efe
himanshu-1034 <himanshu1034.cse18@chitkara.edu.in>
2021-03-03 14:39:30 +0530

    added sign out functionality

--- src/containers/navbar/index.js
+++ src/containers/navbar/index.js
@@ -1,18 +1,22 @@
 import React , {useContext} from 'react'
 import {SignInButton} from '../../components';
 import { UserContext } from '../../contexts/user';
+import { logOut } from '../../services/auth';
 import './style.css';
 export default function Navbar() {
 
     const [user, setUser] = useContext(UserContext).user;
 
-
+    const signOut = () =>{
+        logOut();
+        setUser(null);
+    }
 
 
     return (
         <div className="navbar">
             <p>Our Social Network</p>
-            {user ? <><img className="navbarPhoto" src={user.photoURL}/></> : <><SignInButton/></>}
+            {user ? <div className="signoutDiv"><img className="navbarPhoto" src={user.photoURL}/><button className="signoutbtn" onClick={signOut}>Sign Out</button></div> : <><SignInButton/></>}
         </div>
     )
 }
